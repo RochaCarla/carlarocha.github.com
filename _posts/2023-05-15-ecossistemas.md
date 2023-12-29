17781a0fdaeeffc62f27555eacebc31644152e40
Carla Rocha <seu_email@exemplo.com>
2023-12-29 12:09:22 -0300

    commitao

---
+++ _posts/2023-05-15-ecossistemas.md
@@ -0,0 +1,21 @@
+---
+layout: single
+title: Projeto "Ecossistema de Software Livre - Participação Digital" - 2023 - em andamento
+year: 2024
+month: 05
+day: 15
+published: true
+figure: /images/logo-bp.png
+resume: Projeto em colaboração com a Secretaria Nacional de Participação Social para o desenvolvimento da Plataforma de Participação digital Brasil Participativo, adotando o software Livre Decidim. Em 6 meses de projetos, tivemos as maiores experiências de participação digital tanto no Brasil quanto usando o Decidim
+permalink: /projects/ecossistemas
+---
+
+O Brasil Participativo é um software de código aberto adaptado a partir da plataforma de participação eletrônica Decidim, lançada pela prefeitura de Barcelona em 2016. A Decidim oferece diversos espaços de interação, como iniciativas, assembleias e consultas, possibilitando diferentes formas de engajamento, como enquetes, submissão de propostas, votação, acompanhamento de resultados e comentários.
+
+Fruto de uma colaboração entre o Laboratório Avançado de Produção, Pesquisa e Inovação em Software (Lappis) da Faculdade UnB Gama (FGA) e o governo federal, por meio da Secretaria Nacional de Participação Social da Secretaria-Geral da Presidência da República, o Brasil Participativo proporciona a qualquer pessoa a oportunidade de sugerir políticas públicas a serem incluídas no Plano Plurianual (PPA) e executadas com prioridade nos próximos quatro anos.
+
+[Brasil Participativo](https://brasilparticipativo.presidencia.gov.br)
+
+[Leia Mais](https://noticias.unb.br/117-pesquisa/6549-unb-desenvolve-plataforma-que-ajuda-populacao-a-indicar-prioridades-para-o-governo-federal)
+
+[![Equipe](/images/BP.png)](https://gitlab.com/lappis-unb/decidimbr)
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,55 @@
-var store = []
+var store = [{
+        "title": "Chatbot para Orientar a Submissão de Propostas de Incentivo à Cultura - Ministério da Cultura - 2017-2019",
+        "excerpt":"Neste projeto, desenvolvemos um dos primeiros chatbots para o governo brasileiro. Criamos uma solução de chatbot de código aberto, com mais de 70 intenções para responder sobre a Lei de Incentivo à Cultura, orientando agentes culturais na proposição de seus projetos. Inovamos na arquitetura do chatbot, pipeline de implantação contínua,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/minc",
+        "teaser": null
+      },{
+        "title": "Colaboração em Pesquisa UnB (Brasília), USP (São Paulo), UPM (Madrid) - 2021 - em andamento",
+        "excerpt":"Nossa pesquisa tem como foco principal a investigação empírica da estrutura organizacional e das características das equipes de DevOps em organizações que desenvolvem software. Por meio da colaboração entre três equipes de pesquisa experientes, nosso objetivo é criar uma compreensão abrangente nesse campo de constante evolução. Os resultados deste estudo...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/usp-upm",
+        "teaser": null
+      },{
+        "title": "Residência TIC Brisas UnB - 2022 - em andamento",
+        "excerpt":"Na qualidade de coordenadora do projeto ‘Residência em TIC da BRISA - UnB FGA’, estou liderando uma iniciativa colaborativa entre a BRISA - Sociedade para o Desenvolvimento da Tecnologia da Informação - e a Universidade de Brasília, Campus Gama, em parceria com o LAPPIS. Nosso objetivo é oferecer treinamento especializado...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/brisas",
+        "teaser": null
+      },{
+        "title": "Big Open Source Siblings (BOSS) - 2020 - Em andamento",
+        "excerpt":"A iniciativa BOSS foi projetada para capacitar grupos sub-representados na indústria de tecnologia a aprender e contribuir para projetos de código aberto. Com foco na mentoria, o programa oferece um ambiente de apoio onde os participantes podem desenvolver as habilidades necessárias e a confiança para trabalhar em projetos. O processo...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/boss",
+        "teaser": null
+      },{
+        "title": "Pacote Hygia OSS Python - 2023 - em andamento",
+        "excerpt":"Hygia é um pacote Python poderoso e inovador que simplifica o processo de trabalhar com dados de Machine Learning. Com sua configuração rápida, flexível e intuitiva de pipeline de dados baseada em YAML, o Hygia facilita para desenvolvedores e usuários gerenciar, organizar, comparar e compartilhar todos os metadados de seus...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/hygia",
+        "teaser": null
+      },{
+        "title": "Aprendizado de Máquina para Detecção de Anomalias em Bancos de Dados de Clientes - Parceria com o Instituto Eldorado/Dell - 2021",
+        "excerpt":"Este projeto tem como foco o uso de algoritmos de aprendizado de máquina para detectar anomalias e outliers em grandes conjuntos de dados de clientes. O objetivo é aprimorar a precisão dessas detecções e tornar o processo mais eficiente por meio do uso de dados, engenharia de características e técnicas...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/ml",
+        "teaser": null
+      },{
+        "title": "Projeto \"Ecossistema de Software Livre - Participação Digital\" - 2023 - em andamento",
+        "excerpt":"O Brasil Participativo é um software de código aberto adaptado a partir da plataforma de participação eletrônica Decidim, lançada pela prefeitura de Barcelona em 2016. A Decidim oferece diversos espaços de interação, como iniciativas, assembleias e consultas, possibilitando diferentes formas de engajamento, como enquetes, submissão de propostas, votação, acompanhamento de...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/ecossistemas",
+        "teaser": null
+      },{
+        "title": "E-book \"Como Acelerar o Aprendizado e Disseminar a Cultura de Inovação Ágil\"",
+        "excerpt":"Estou escrevendo um livro!!! Coloco aqui em projetos, pois a escrita de um livro é um super projeto em si. A ideia desse livro veio de anos de experiência aplicando técnicas de aprendizagem por experiência, mentoria, onboarding em comunidades de software livre e desenvolvimento de software. A metodologia foi refinada...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/e-book",
+        "teaser": null
+      },{
+        "title": "Dinheiro Público Código Público – Modernizando a Infraestrutura Pública com Software Livre",
+        "excerpt":"A equipe LAPPIS traduziu para o português brasileiro a publicação “Dinheiro Público Código Público – Modernizando a Infraestrutura Pública com Software Livre” da Free Software Foundation Europe. A tradução em português tem o objetivo de conscientizar sobre os benefícios do uso de software livre na infraestrutura pública, especialmente no contexto...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/traducao",
+        "teaser": null
+      }]
